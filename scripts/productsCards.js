bef3c2742b9a298ec61e260fe111524e7f5b944f
AndrySmiles <caramu@yandex.ru>
2021-07-07 14:46:54 +0300

    Добавил фалы ДЗ
    Работающий вариант

---
+++ scripts/productsCards.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const pathToImages = 'img';
+const pathToProductsImages = `${pathToImages}/products`;
+const featuredItemsEl = document.querySelector('.featuredItems');
+
+const getProductMarkup = (product) => {
+    return `   
+    <div class="card card__box__link" onclick="location.href='product.html';">
+    <div class="box__card__img">
+        <img src="${pathToProductsImages}/${product.image}" alt="${product.name}">
+        <div class="card__product__buttons">
+            <button class="card__button" data-productId="${product.id}">                
+                Add to Cart
+            </button>
+        </div>
+    </div>
+    <h4 class="heading__mini">${product.name}</h4>
+    <p class="card__text">${product.description}</p>
+    <p class="card__price">$${product.price}</p>
+    </div>    
+    `;
+};
+
+const insertProductsIntoPage = (list = products) => {
+    let productsMarkup = list.map(
+        product => getProductMarkup(product)
+    ).join('');
+
+    featuredItemsEl.insertAdjacentHTML('afterbegin', productsMarkup);
+}
+
+insertProductsIntoPage(products);
